042d01286d8d5187aa81eeed52aa7541998f46ed
dakotabenjamin <dakota.benjamin@gmail.com>
2014-06-10 08:41:44 -0400

    using full model

--- topoWetness.R
+++ topoWetness.R
@@ -2,8 +2,7 @@
 # Cleveland Metroparks
 # dmb2@clevelandmetroparks.com
 
-#-----
-#Some initialization
+# initialization ----
 
 #Clear Env
 rm(list=ls())
@@ -13,11 +12,9 @@ library(randomForest) #for the random forest
 library(sp) #Classes and methods for spatial data
 library(rgdal) #bindings for GDAL
 
-setwd("C:/Users/Dakota/Documents/RandomForest/RandomForest")
+#setwd("~/randomForest")
 
-# ----
-# Get and format data
-# ----
+# Get and format data ----
 
 topo <- readOGR("topo", "ms-test-points-all")
 
@@ -37,16 +34,17 @@ topo.data$unique_id <- NULL
 #for any point (row) with NA, delete that point
 topo.data <- topo.data[complete.cases(topo.data),]
 
-#topo.matrix <- cbind(topo.coords,topo.data)
+# RandomForest ----
+model <- topo.data$gid ~ topo.data$area_acres * topo.data$poly_type * topo.data$ms_test__1 * topo.data$ms_test__2 * topo.data$ms_test__3 * topo.data$ms_test_lo * topo.data$ms_test_co * topo.data$ms_test_as * topo.data$ms_test_xs * topo.data$ms_test_sl * topo.data$ms_test_ch * topo.data$ms_test_ls * topo.data$ms_test_to * topo.data$ms_test_va * topo.data$ms_test_ca * topo.data$ms_test_ve * topo.data$ms_test_re
 
-set.seed(352)
-topo.rf <- randomForest(topo.data$gid ~ ., data=topo.data, mtry=5, importance=T, do.trace=100)
+set.seed(17)
+topo.rf <- randomForest(topo.data$gid ~ topo.data$area_acres * topo.data$poly_type * topo.data$ms_test__1 * topo.data$ms_test__2 * topo.data$ms_test__3 * topo.data$ms_test_lo * topo.data$ms_test_co * topo.data$ms_test_as * topo.data$ms_test_xs * topo.data$ms_test_sl * topo.data$ms_test_ch * topo.data$ms_test_ls * topo.data$ms_test_to * topo.data$ms_test_va * topo.data$ms_test_ca * topo.data$ms_test_ve * topo.data$ms_test_re, importance=T, do.trace=100)
 
 print(topo.rf)
 
 par(mfrow=c(2,1))
 for (i in 1:2) {
-  plot(sort(topo.rf$importance[,i], dec = T),
+  pl1 <- plot(sort(topo.rf$importance[,i], dec = T),
        type="h", xaxt="n", main=paste("Measure ", i))
-  axis(1, at=1:19, labels=names(topo.rf$forest$xlevels))
-}
+  axis(1, at=1:19, labels=names(topo.rf$forest$xlevels), las=3)
+  }
